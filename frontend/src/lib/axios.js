bcfd6b62b41e831f6305df546297ef4d99d8619a
Mankirat364 <mankirat.techguild@gmail.com>
2025-04-16 13:47:03 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL : 'http://localhost:3000/api',
+    baseURL : 'https://mern-chat-web-app-backend.onrender.com/api',
     withCredentials: true,
 })
