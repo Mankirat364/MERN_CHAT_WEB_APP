ee88c27b907254d6d622a5a8b82e829166f7bfdd
Mankirat364 <mankirat.techguild@gmail.com>
2025-04-16 14:22:17 +0530

    Update utils.js

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
-const generateToken = async (userId, res) => {
+export const generateToken = async (userId, res) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
         expiresIn: "7d",
     });
