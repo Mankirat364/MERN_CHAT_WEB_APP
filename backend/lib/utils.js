041bbfa24faf4b7097186d828a9e843e881d7383
Mankirat364 <mankirat.techguild@gmail.com>
2025-04-16 14:11:40 +0530

    Update utils.js

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -1,15 +1,17 @@
 import jwt from 'jsonwebtoken'
-export const generateToken = async(userId,res) =>{
-    const token = jwt.sign({userId},process.env.JWT_SECRET, {
-        expiresIn : "7d",
-
-    })
-
-    res.cookie("jwt",token,{
-        maxAge : 7*24*60*60*1000,
-        httpOnly : true,
-        sameSite : "strict",
-        secure : process.env.NODE_ENV !== "development"
+export const generateToken = async (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
     });
-    return token
+
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        httpOnly: true,
+        sameSite: process.env.NODE_ENV === "production" ? "Strict" : "Lax", // "Lax" for development
+        secure: process.env.NODE_ENV === "production", // only true in production
+    });
+
+    return token;
+};
+
 }
