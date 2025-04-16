ca88b951d1072c6a9da6bba2f2abc5ea72f1db61
Mankirat364 <mankirat.techguild@gmail.com>
2025-04-16 14:15:48 +0530

    Update utils.js

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -7,8 +7,8 @@ export const generateToken = async (userId, res) => {
     res.cookie("jwt", token, {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
         httpOnly: true,
-        sameSite: process.env.NODE_ENV === "production" ? "Strict" : "Lax", // "Lax" for development
-        secure: process.env.NODE_ENV === "production", // only true in production
+        sameSite: "None",
+        secure:"true"
     });
 
     return token;
